eef9f4b5f35d462726d6b810feec435c41564c6c
aditya <adityapatwari19@gmail.com>
2023-09-02 20:12:02 +0530

    finished project

--- app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -5,21 +5,21 @@ public class QuestionAnswer {
     public static String question[] ={
             "Which company owns the android?",
             "Which one is not the programming language?",
-            "Where you are watching this video?",
+            "Which programming language was invented by SunMicroSystems",
             "Which company owns the Apple?"
     };
 
     public static String choices[][] = {
             {"Google","Apple","Nokia","Samsung"},
             {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Java","Python","C++","Javascript"},
             {"Google","Apple","Nokia","Samsung"}
     };
 
     public static String correctAnswers[] = {
             "Google",
             "Notepad",
-            "Youtube",
+            "Java",
             "Apple"
     };
 
